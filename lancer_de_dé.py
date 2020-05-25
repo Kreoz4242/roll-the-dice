9d3e43a18096d880cb5877e283c60ff9650a151b
Kreoz <65891504+Kreoz4242@users.noreply.github.com>
2020-05-25 10:39:19 +0200

    Update lancer_de_dé.py
    translate

--- lancer_de_dé.py
+++ lancer_de_dé.py
@@ -3,15 +3,15 @@ import random
 import os
 
 #set value====================
-nbd = int(input("entrez votre nombre de dé: "))
-nbf = int(input("entrez un nombre de faces: "))
+nbd = int(input("enter your number of dice: "))
+nbf = int(input("enter a number of faces: "))
 somme = 0
 ListePlace = 0
 
 #set list====================
 ListeDe = []
 
-#lancement de dé====================
+#roll the dice====================
 for i in range(nbd):
     lancer = random.randint(1, nbf)
     ListeDe.append(lancer)
@@ -19,17 +19,17 @@ for i in range(nbd):
 #clear====================
 os.system("cls")
 
-#print l'interieur de la liste ListePlace====================
-print("Les dés sont: ", end="")
+#print in the list ListePlace====================
+print("dice: ", end="")
 for i in range(nbd):
     print(ListeDe[int(ListePlace)], end=" ")
     ListePlace += 1
 
-#print de la somme des dés====================
+#print the sum of the dice====================
 ListePlace = 0
 
 for i in range(nbd):
     somme += ListeDe[int(ListePlace)]
     ListePlace += 1
 
-print("\nLa somme des dés est de {}".format(somme))
+print("\nThe sum of the dice is {}".format(somme))
